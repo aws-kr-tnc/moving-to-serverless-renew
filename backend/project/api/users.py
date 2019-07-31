98d664cba44c310519b7947b3f95123598c61de3
dayoungles <dayoungles@gmail.com>
2019-07-31 10:45:10 +0900

    function: email&pw data validation/define response code

--- backend/project/api/users.py
+++ backend/project/api/users.py
@@ -3,13 +3,13 @@ from flask import Blueprint, request
 from flask import current_app as app
 from flask_restful import Resource, Api
 from flask_login import current_user, login_user, logout_user
-
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from sqlalchemy import exc
 from project import db
 from project.api.models import User
 from project import login
+import re
 
 users_blueprint = Blueprint('users', __name__)
 api = Api(users_blueprint)
@@ -17,8 +17,7 @@ api = Api(users_blueprint)
 
 class UsersPing(Resource):
     def get(self):
-        return util_create_response('success', 'pong!')
-
+        return response_util(200, 'pong!')
 
 class UsersList(Resource):
     def get(self):
@@ -28,8 +27,7 @@ class UsersList(Resource):
                 'users': [user.to_json() for user in User.query.all()]
             }
         }
-        return util_create_response('success', msg), 200
-
+        return response_util(200, msg)
 
 class Users(Resource):
     def get(self, user_id):
@@ -37,8 +35,8 @@ class Users(Resource):
 
         try:
             user = User.query.filter_by(id=int(user_id)).first()
-            if not user:
-                return util_create_response('fail', 'User does not exist'), 404
+            if user is None:
+                return response_util(404)
             else:
                 msg = {
                     'data': {
@@ -47,36 +45,43 @@ class Users(Resource):
                         'email': user.email
                     }
                 }
-                return util_create_response('success', msg), 200
+                return response_util(200, msg)
         except ValueError:
-            return util_create_response('fail', 'User does not exist'), 404
+            return response_util(404)
 
 class Signup(Resource):
     def post(self):
         post_data = request.get_json()
 
         if not post_data:
-            return util_create_response('fail', 'Invalid payload'), 400
+            return response_util(400)
 
         username = post_data.get('username')
         email = post_data.get('email')
         password = post_data.get('password')
 
         if None in (username, email, password):
-            return util_create_response('fail', 'Invalid payload'), 400
+            return response_util(400)
+
+        validated = data_validate(email, password)
+
+        if not validated:
+            return response_util(400, 'email or password not valid')
 
         try:
             user = User.query.filter_by(email=email).first()
             if not user:
                 db.session.add(User(username=username, email=email, password=generate_password_hash(password)))
                 db.session.commit()
-                return util_create_response('success', '%s was added!' % email), 201
+                return response_util(201, '%s was added!' % email)
             else:
-                return util_create_response('fail', 'Sorry. This email already exists.'), 400
-
+                return response_util(400, 'Sorry. This email already exists.')
+        except ValueError:
+            db.session.rollback()
+            return response_util(500, 'Undefined status code.')
         except exc.IntegrityError:
             db.session.rollback()
-            return util_create_response('fail', 'Invalid payload'), 400
+            return response_util(500)
 
 class Signin(Resource):
     def post(self):
@@ -86,43 +91,70 @@ class Signin(Resource):
         validated = data_validate(email, password)
 
         if not validated:
-            return util_create_response('fail', 'Invalid user data'), 400
+            return response_util(400, 'email or password not valid')
 
         db_user = db.session.query(User).filter_by(email=email).first()
 
         if db_user and check_password_hash(db_user.password, password):
             app.logger.debug("login success: id: %s | email: %s" % (db_user.id, db_user.email))
             login_user(db_user)
-            return util_create_response('success', 'Login success'), 200
+            return response_util(200)
         else:
             app.logger.debug('user login failed: %s' % db_user)
-            return util_create_response('fail', 'Login failed'), 400
+            return response_util(400, "Login Failed")
 
 
 class Signout(Resource):
     def post(self):
         app.logger.debug('Sign-out : %s', current_user.username)
         logout_user()
-        return util_create_response('success', 'Signout success.')
+        return response_util(200)
 
 @login.user_loader
 def user_loader(user_id):
     result = User.query.get(user_id)
-    if result is not None:
-        return result
-    return None
+    if not result:
+        return None
+    return result
 
+dic_response = {200 : { 'status': 200,
+                         'message': 'Success'},
+                201 : { 'status': 201,
+                         'message': 'Created'},
+                400 : { 'status': 400,
+                         'message': 'Invalid payload'},
+                404 : { 'status': 404,
+                         'message':'User not exist'},
+                500 : { 'status': 500,
+                         'message': 'Internal server error'}
+                 }
 
+def response_util(response_code):
+    try:
+        if response_code in dic_response:
+            return dic_response[response_code], response_code
+        else:
+            raise ValueError
+    except:
+        raise ValueError
 
-def util_create_response(status, msg):
-    return {
-        'status': status,
-        'message': msg
-    }
+def response_util(response_code, msg):
+    try:
+        if response_code in dic_response:
+            res = dic_response[response_code]
+            res['message'] = msg
+            return res, response_code
+        else:
+            raise ValueError
+    except:
+        app.logger.debug("Undefined error: %s" % response_code)
+        raise ValueError
 
 
 def data_validate(email, password):
-    # TODO implement
+    email_regex = re.compile(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$")
+    if not re.match(email_regex, email) or (len(password) <= 1):
+        return False
     return True
 
 
