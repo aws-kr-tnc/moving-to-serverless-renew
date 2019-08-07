db33fcd5ae0acee02522a6f1877613e757a8c6b7
Sungshik Jou <liks79@gmail.com>
2019-08-07 11:06:28 +0900

    Fix signin bug

--- backend/project/api/users.py
+++ backend/project/api/users.py
@@ -165,9 +165,9 @@ class Signin(Resource):
             if post_data['ok']:
                 data = post_data['data']
                 user = db.session.query(User).filter_by(email=data['email']).first()
-                user = user.to_json()
 
-                if user and check_password_hash(user['password'], data['password']):
+                if user is not None and check_password_hash(user.password, data['password']):
+                    user = user.to_json()
                     del user['password']
                     access_token = create_access_token(identity=data)
                     refresh_token = create_refresh_token(identity=data)
