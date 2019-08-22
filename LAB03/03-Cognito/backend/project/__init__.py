214ffaca923a47445a766e53f67838d7d82cc885
dayoungles <dayoungles@gmail.com>
2019-08-22 20:23:08 +0900

    lab03-03cognito fixed

--- LAB03/03-Cognito/backend/project/__init__.py
+++ LAB03/03-Cognito/backend/project/__init__.py
@@ -6,7 +6,6 @@ import datetime
 
 from bson.objectid import ObjectId
 from flask import Flask, jsonify, make_response  # new
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_login import LoginManager
 from flask_jwt_extended import JWTManager
@@ -25,8 +24,6 @@ class JSONEncoder(json.JSONEncoder):
             return str(o)
         return json.JSONEncoder.default(self, o)
 
-# instantiate the db
-db = SQLAlchemy()
 login = LoginManager()
 jwt = JWTManager()
 
@@ -59,26 +56,12 @@ def create_app(script_info=None):
     app.logger.addHandler(logging.StreamHandler(sys.stdout))
     app.logger.setLevel(logging.DEBUG)
 
-    # set up extensions
-    # db.init_app(app)
-
     # register blueprints
     from project.api.users import users_blueprint
     app.register_blueprint(users_blueprint, url_prefix='/users')
 
     from project.api.photos import photos_blueprint
     app.register_blueprint(photos_blueprint, url_prefix='/photos')
-
-    from project.api.map import map_blueprint
-    app.register_blueprint(map_blueprint)
-
-    # Setup models for DB operations
-    with app.app_context():
-        try:
-            # db.create_all()
-            pass
-        except Exception as e:
-            app.logger.error(e)
 
 
     @jwt.token_in_blacklist_loader
@@ -89,7 +72,6 @@ def create_app(script_info=None):
         except Exception as e:
             app.logger.error(e)
             return make_response(jsonify({'msg': 'session already expired'}, 409))
-
 
     # shell context for flask cli
     @app.shell_context_processor
