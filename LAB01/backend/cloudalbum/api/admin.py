1e8f1cc9287f89232c681c9bad44fa5410b0c446
dayoungles <dayoungles@gmail.com>
2019-08-28 10:05:45 +0900

    health_check code modified by sungshik

--- LAB01/backend/cloudalbum/api/admin.py
+++ LAB01/backend/cloudalbum/api/admin.py
@@ -21,37 +21,23 @@ class Ping(Resource):
         return m_response(True, {'msg':'pong!'}, 200)
 
 
+
 @api.route('/health_check')
 class HealthCheck(Resource):
     @api.doc(responses={200: 'system alive!'})
     def get(self):
-        status = True
         try:
-            # DB health
-            res = db.engine.execute('SELECT 1')
-            if res is None:
-                app.logger.debug("db not answered!")
-                status = False
-
-            # Disk health
+            # 1. Is DB is responsive?!
+            db.engine.execute('SELECT 1')
+            # 2. Is disk have enough free space?!
             total, used, free = shutil.disk_usage("/")
-
             if used / total * 100 >= 90:
-                app.logger.debug("free disk size under 10%")
-                status = False
-
-            #health check result
-            if status is False:
-                return m_response(False, {'msg':'healthcheck failed'},500)
-
-
-
-            app.logger.debug("success:db alive!")
-            app.logger.debug("success:health check!")
+                raise Exception("free disk size under 10%")
+            # 3. Something else..
+            # TODO: health check something
             return m_response(True, {'msg':'health_check success', "hostname": get_ip_addr()}, 200)
         except Exception as e:
             app.logger.error(e)
-            app.logger.error("db not answerd")
             return m_response(False, {'msg': 'healthcheck failed', "hostname": get_ip_addr()}, 500)
 
 
