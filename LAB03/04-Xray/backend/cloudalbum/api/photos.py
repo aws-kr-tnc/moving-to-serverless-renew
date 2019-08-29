899cdd50a4664a435dc6616cf6b971c8412f5f6c
dayoungles <dayoungles@gmail.com>
2019-08-29 14:53:22 +0900

    xray response message modified

--- LAB03/04-Xray/backend/cloudalbum/api/photos.py
+++ LAB03/04-Xray/backend/cloudalbum/api/photos.py
@@ -140,7 +140,7 @@ class List(Resource):
                 data['photos'].append(url)
 
             app.logger.debug("success:photos_list:%s" % data)
-            return m_response(data, 200)
+            return m_response(data['photos'], 200)
 
         except Exception as e:
             app.logger.error("ERROR:photos list failed")
