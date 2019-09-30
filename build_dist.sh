38f73f2edb8c2f2c568b60b4abf222a6a70f34d2
Sungshik Jou <liks79@gmail.com>
2019-09-30 14:13:14 +0900

    Fix build_sist.sh script

--- build_dist.sh
+++ build_dist.sh
@@ -3,16 +3,16 @@ WORK_DIR=/tmp/cloudalbum_dist
 CWD=`pwd`
 echo "building.. './resources/cloudalbum-mts-dist-src.zip' file. "
 echo "-----------------------------------------------------------"
-mkdir -p ${WORK_DIR}/frontend/cloudalbum
-rsync -q -av --progress ./LAB01/frontend/cloudalbum ${WORK_DIR}/frontend/ --exclude node_modules --exclude dist
-mkdir -p ${WORK_DIR}/LAB01/backend
-cp -r ./LAB01/backend/ ${WORK_DIR}/LAB01/
-mkdir -p ${WORK_DIR}/LAB02/backend
-cp -r ./LAB01/backend/ ${WORK_DIR}/LAB02/
-mkdir -p ${WORK_DIR}/LAB03/
-cp -r ./LAB03/ ${WORK_DIR}/LAB03/
-mkdir -p ${WORK_DIR}/LAB04/
-cp -r ./LAB04/ ${WORK_DIR}/LAB04/
+mkdir -p ${WORK_DIR}/cloudalbum-mts-dist-src/cloudalbum-mts-dist-src/frontend/cloudalbum
+rsync -q -av --progress ./LAB01/frontend/cloudalbum ${WORK_DIR}/cloudalbum-mts-dist-src/frontend/ --exclude node_modules --exclude dist
+mkdir -p ${WORK_DIR}/cloudalbum-mts-dist-src/LAB01/backend
+cp -r ./LAB01/backend/ ${WORK_DIR}/cloudalbum-mts-dist-src/LAB01/
+mkdir -p ${WORK_DIR}/cloudalbum-mts-dist-src/LAB02/backend
+cp -r ./LAB01/backend/ ${WORK_DIR}/cloudalbum-mts-dist-src/LAB02/
+mkdir -p ${WORK_DIR}/cloudalbum-mts-dist-src/LAB03/
+cp -r ./LAB03/ ${WORK_DIR}/cloudalbum-mts-dist-src/LAB03/
+mkdir -p ${WORK_DIR}/cloudalbum-mts-dist-src/LAB04/
+cp -r ./LAB04/ ${WORK_DIR}/cloudalbum-mts-dist-src/LAB04/
 # Remove temporary files.
 find ${WORK_DIR} -name ".pytest_cache" -exec rm -rf {} \; 2>/dev/null
 find ${WORK_DIR} -name "__pycache__" -exec rm -rf {} \; 2>/dev/null
