0eb6c37445a00ad322e4596aceeff9f1ad04972f
YoungSeon.Ahn <ys.ahn@outlook.com>
2020-10-19 14:14:33 +0900

    map-dialog unit test case

---
+++ LAB01/frontend/cloudalbum/tests/unit/components/map/MapDialog.spec.js
@@ -0,0 +1,10 @@
+import { shallowMount } from "@vue/test-utils";
+import MapDialog from "@/components/map/MapDialog"
+
+describe("MapDialog", () => {
+  const wrapper = shallowMount(MapDialog);
+
+  it("renders v-dialog", () => {
+    expect(wrapper.find("v-dialog-stub").exists()).toBe(true);
+  });
+});
